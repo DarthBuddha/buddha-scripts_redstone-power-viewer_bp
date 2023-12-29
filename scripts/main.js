fec4d3ed5f1646b46187ee149b2cde0faf05ccce
Darth Buddha <ltm6942@hotmail.com>
2023-12-28 21:59:59 -0500

    0.0.1

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+import * as mc from "@minecraft/server";
+
+mc.system.runInterval(() => {
+    const players = mc.world.getAllPlayers();
+    for (let player of players) {
+        const blockHit = player.getBlockFromViewDirection();
+
+        // Ensure blockHit is valid
+        if (!blockHit) {
+            // Reset lastRedstonePower if no block is in view
+            if (player.lastRedstonePower !== undefined) {
+                player.onScreenDisplay.setActionBar("");
+                player.lastRedstonePower = undefined;
+            }
+            continue;
+        }
+
+        const block = blockHit.block;
+        const redstonePower = block.getRedstonePower();
+
+        // Update action bar only if redstone power has changed
+        if (player.lastRedstonePower !== redstonePower) {
+            if (redstonePower >= 0) {
+                player.onScreenDisplay.setActionBar(`${redstonePower}`);
+            } else {
+                // Clear the action bar if no redstone power
+                player.onScreenDisplay.setActionBar("");
+            }
+            player.lastRedstonePower = redstonePower;
+        }
+    }
+}, 2);
